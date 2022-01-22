1a6087d6d5c6bbff29b1d9ad2677727896d4dbc6
Undisclosed <undisclosed674@gmail.com>
2022-01-22 02:17:03 -0800

    add Home component

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home(){
+return(
+    <div>
+    <h1>The free resume builder</h1>
+    <p>It will create amzing resume for you through which you can be ahead in the game. Stop using 80's resume and create a modern looking resume now with the simplest steps for today's generation,by today's tool. </p>
+    </div>
+)
+}
+export default Home;
