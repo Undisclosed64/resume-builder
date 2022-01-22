ac16ccb7c23ebf9ac551304cacbe52566bb96f1b
Undisclosed <undisclosed674@gmail.com>
2022-01-22 03:05:33 -0800

    add home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 function Home(){
 return(
-    <div>
-    <h1>The free resume builder</h1>
-    <p>It will create amzing resume for you through which you can be ahead in the game. Stop using 80's resume and create a modern looking resume now with the simplest steps for today's generation,by today's tool. </p>
+    <div id="home">
+    <h1 className='home-heading'>The free resume builder</h1>
+    <p className='homeParagraph'>It will create amzing resume for you through which you can be ahead in the game. Stop using 80's resume and create a modern looking resume now with the simplest steps for today's generation,by today's tool. </p>
+    <button className='createResumeBtn'>Create my resume now</button>
     </div>
 )
 }
