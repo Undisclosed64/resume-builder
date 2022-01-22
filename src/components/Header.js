cdcc612bb8fbb5126ac176ea68e0401887d77852
Undisclosed <undisclosed674@gmail.com>
2022-01-21 22:34:59 -0800

    add toggle function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
+import {FaTools} from "react-icons/fa"
 
 export default class Header extends Component {
+
+    toggleNavBar = () => {
+        const navLinkList = document.querySelector('.UlNavList');
+        navLinkList.classList.toggle('active');
+    }
+    
     render() {
         return (
             <div>
                 <nav>
-                   <div className="brandName">Resume Builder</div>
-                   <ul className="navLinksList">
-                       <li className="navbarLink">Home</li>
+                   <div className="brandName"><FaTools className='logo'/>Resume Builder</div>
+
+                   <div className='toggleButton'onClick={this.toggleNavBar}>
+<span className='bar'></span>
+<span className='bar'></span>
+<span className='bar'></span>
+                   </div>
+                   
+                   <ul className="UlNavList">
+                       <li className="navbarLink">Service</li>
                        <li className="navbarLink">About</li>
                        <li className="navbarLink">contact</li>
 
